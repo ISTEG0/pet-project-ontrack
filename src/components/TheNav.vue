ba9a4246ea5ee9de4c00bbaeebd9c4f0731c7754
Ivan Marahovskii <marahovskii81@gmail.com>
2023-06-25 18:22:11 +0500

    normalize hash page

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,15 +1,28 @@
 <script setup>
 import { ref } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constants.js'
 import NavItem from './NavItem.vue'
 
 const navItems = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon
 }
 
-const currentPage = ref('timeline');
+const currentPage = ref(normalizePageHash())
+
+function normalizePageHash() {
+    const hash = window.location.hash.slice(1);
+
+    if (Object.keys(navItems).includes(hash)) {
+        return hash;
+    }
+
+    window.location.hash = PAGE_TIMELINE;
+
+    return PAGE_TIMELINE;
+}
 </script>
 
 <template>
@@ -19,7 +32,7 @@ const currentPage = ref('timeline');
         v-for="(icon, page) in navItems"
         v-bind:key="page"
         v-bind:href="`#${page}`"
-        v-bind:class="{ 'bg-gray-200 pointer-events-none': page === currentPage }"
+        v-bind:class="{ 'pointer-events-none bg-gray-200': page === currentPage }"
         v-on:click="currentPage = page"
       >
         <component v-bind:is="icon" class="h-6 w-6" /> {{ page }}
